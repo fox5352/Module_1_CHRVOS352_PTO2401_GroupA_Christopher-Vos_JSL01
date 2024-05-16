a79caf16262a47f5cad5c4a287bff4d4dc3753e9
christopher <christophervos522@gmail.com>
2024-05-16 13:36:15 +0200

    added pattern matching and unicode characters

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
+    const pattern = new RegExp("pet_\\w+")
+
+    const isValid = input.search(pattern);
+
     let result = ''; // Placeholder for validation result
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+    if (isValid !== -1) {
+        result = `Valid Syntax <span class="invalid">&#128994;</span>`;
+    }else{
+        result = `Invalid Syntax <span class="invalid">&#128308;</span>`;
+    }
 
-            document.getElementById('result').innerText = result;
+    // Check if input starts with 'pet_' and followed by alphanumeric characters
+    document.getElementById('result').innerHTML = result;
 }
 
 
